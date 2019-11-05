cf7e35f1c500eb37f69cce3e042a326073370ef1
Damien Mortini <ddotmnn+git@gmail.com>
2019-11-05 20:20:50 +0900

    put links element back in graph

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import Config from "./Config.js";
+import Config from './Config.js';
 
 let initialized = false;
 
 export default class GraphElement extends HTMLElement {
   static get observedAttributes() {
-    return ["config"];
+    return ['config'];
   }
 
   static initialize(configUrl) {
     if (initialized) {
-      console.warn("Graph has already been initialized with a config file.");
+      console.warn('Graph has already been initialized with a config file.');
       return Promise.resolve();
     }
     initialized = true;
@@ -28,7 +28,7 @@ export default class GraphElement extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: "open" }).innerHTML = `
+    this.attachShadow({ mode: 'open' }).innerHTML = `
       <style>
         :host {
           display: block;
@@ -49,18 +49,42 @@ export default class GraphElement extends HTMLElement {
     customElements.whenDefined('graph-viewport').then(() => {
       this.shadowRoot.querySelector('graph-viewport').childrenDragAndDropExceptions.push((nodes) => {
         for (const node of nodes) {
-          if ((node.nodeName === 'INPUT' || node.nodeName === 'BUTTON' || node.nodeName === 'TEXTAREA') && !node.disabled) {
+          if ((node.nodeName === 'NODE-CONNECTOR-INPUT' || node.nodeName === 'INPUT' || node.nodeName === 'BUTTON' || node.nodeName === 'TEXTAREA') && !node.disabled) {
             return true;
           }
         }
         return false;
       });
-    })
+    });
+
+    const currentViewport = this.shadowRoot.querySelector('graph-viewport');
+    let currentLink;
+    const onLink = (event) => {
+      if (currentLink) {
+        if (event.detail.input && event.detail.output) {
+          currentLink.input = event.detail.input;
+          currentLink.output = event.detail.output;
+        } else {
+          currentLink.remove();
+        }
+        currentLink = null;
+      } else {
+        currentLink = document.createElement('graph-link');
+        currentViewport.prepend(currentLink);
+        currentLink.input = event.composedPath()[0];
+        currentLink.addEventListener('click', (event) => {
+          event.currentTarget.input.outputs.delete(event.currentTarget.output);
+          event.currentTarget.remove();
+        });
+      }
+    };
+    this.addEventListener('linkstart', onLink);
+    this.addEventListener('linkend', onLink);
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     switch (name) {
-      case "config":
+      case 'config':
         GraphElement.initialize(newValue);
         break;
     }
