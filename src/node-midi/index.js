f2d779b5aae0e1e761eba520830c56ea04811d94
Damien Mortini <ddotmnn+git@gmail.com>
2020-01-17 19:25:48 +0100

    add node-midi

--- src/node-midi/index.js
+++ src/node-midi/index.js
@@ -1,4 +1,4 @@
-customElements.define('node-midi', class NodeMidi extends HTMLElement {
+customElements.define('graph-node-midi', class NodeMidi extends HTMLElement {
   constructor() {
     super();
 
@@ -7,5 +7,24 @@ customElements.define('node-midi', class NodeMidi extends HTMLElement {
         <graph-input-range name="Input"></graph-input-range>
       </graph-node>
     `;
+
+    console.log(navigator.requestMIDIAccess);
+
+    navigator.requestMIDIAccess()
+      .then(function (access) {
+        const inputs = access.inputs.values();
+
+        for (const input of inputs) {
+          console.log(input);
+          input.addEventListener('midimessage', (event) => {
+            console.log(event.data);
+          })
+        }
+
+        access.onstatechange = function (e) {
+          // Print information about the (dis)connected MIDI controller
+          console.log(e.port.name, e.port.manufacturer, e.port.state);
+        };
+      });
   }
 });
